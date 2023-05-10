101ba5e9507d1253186b11a3e0ae4ff4f3536214
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-10 19:10:51 +0200

    jest setting

---
+++ testing/addList.test.js
@@ -0,0 +1,13 @@
+import { addTask} from '../src/modules/operations.js';
+
+describe("Add a task", () => {
+    beforeEach(() => {
+
+    });
+
+    test("add a task", () => {
+        expect(
+            addTask
+        )
+    })
+})
